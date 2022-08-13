269e5d3713cd4ef99ce45ec5d5b845c1c3a2691f
oliverxuan <2373734127@qq.com>
2022-08-13 18:05:52 +0800

    feat:[init]

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="container">
+		<h1>123</h1>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+</script>
+
+<style lang="scss" scoped>
+</style>
